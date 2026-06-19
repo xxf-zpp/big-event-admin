4ff80a137c790bec01e8591527fd2708790411ea
Xu <2265035472@qq.com>
2026-06-19 16:47:58 +0800

    pinia和element-plus使用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from './stores/user'
+
+const userStore = useUserStore()
+</script>
 
 <template>
   <div>测试</div>
+  <el-button type="primary">确认</el-button>
+  <h1>{{ userStore.token }}</h1>
+  <el-button @click="userStore.setToken('112ddwefwfwefwef')">登录</el-button>
+  <el-button @click="userStore.removeToken()">退出</el-button>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
+import persist from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(persist))
 app.use(router)
 
 app.mount('#app')
