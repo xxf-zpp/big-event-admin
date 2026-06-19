c6e707b90f2706f070ce0c571f4c72d52c928cfa
Xu <2265035472@qq.com>
2026-06-19 16:20:26 +0800

    目录资料添加

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <div>测试</div>
 </template>
 
 <style scoped></style>
