d90c18b5c0a2f0b9ad885fc35c738b908cf51862
Xu <2265035472@qq.com>
2026-06-19 16:09:47 +0800

    E-P代码风格调整

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,12 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{vue,js,mjs,jsx}'],
+    files: ['**/*.{vue,js,mjs,jsx}']
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -16,9 +17,20 @@ export default defineConfig([
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
+        ...globals.browser
+      }
     },
+    rules: {
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+      'no-undef': 'error'
+    }
   },
 
   js.configs.recommended,
@@ -26,5 +38,20 @@ export default defineConfig([
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
-  skipFormatting,
+  pluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true, // 单引号
+          semi: false, // 无分号
+          printWidth: 80, // 每行宽度至多80字符
+          trailingComma: 'none', // 不加对象|数组最后逗号
+          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+        }
+      ]
+    }
+  },
+  skipFormatting
 ])
